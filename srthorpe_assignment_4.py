d40ac1b248ee51705e6c03f5f5fa10929a9aefc9
Steven Thorpe <srthorpe@aggies.ncat.edu>
2025-09-26 17:29:28 -0400

    Create srthorpe_assignment_4.py

---
+++ srthorpe_assignment_4.py
@@ -0,0 +1,11 @@
+student_name = "Steven Thorpe"
+current_gpa = 3.9
+study_hours = 10
+social_points = 40
+stress_level = 0
+
+print(student_name)
+print(current_gpa)
+print(study_hours)
+print(social_points)
+print(stress_level)
